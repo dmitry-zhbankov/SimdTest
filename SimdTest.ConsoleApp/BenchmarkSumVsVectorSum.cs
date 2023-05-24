0365d4cbf91be48ee4bf301bfa7f6e71cc9a24f2
Dmitry Zhbankov <dmitry.zhbankov@gmail.com>
2023-05-25 00:41:34 +0200

    Add benchmarks

---
+++ SimdTest.ConsoleApp/BenchmarkSumVsVectorSum.cs
@@ -0,0 +1,61 @@
+﻿using BenchmarkDotNet.Attributes;
+using SimdTest.Lib;
+
+namespace SimdTest.ConsoleApp;
+
+[RPlotExporter]
+public class BenchmarkSumVsVectorSum
+{
+    [Params(100, 10000, 1000000)] 
+    public int N;
+
+    private int[] intArr;
+    private long[] longArr;
+    private float[] floatArr;
+    private double[] doubleArr;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var random = new Random();
+
+        intArr = new int[N];
+        longArr = new long[N];
+        doubleArr = new double[N];
+        floatArr = new float[N];
+
+        for (int i = 0; i < intArr.Length; i++)
+        {
+            var randNum = random.Next(1, 10);
+
+            intArr[i] = randNum;
+            longArr[i] = randNum;
+            doubleArr[i] = randNum;
+            floatArr[i] = randNum;
+        }
+    }
+
+    [Benchmark]
+    public int IntSum() => intArr.Sum();
+
+    [Benchmark]
+    public int IntVectorSum() => VectorMath.VectorSum(intArr);
+
+    [Benchmark]
+    public long LongSum() => longArr.Sum();
+
+    [Benchmark]
+    public long LongVectorSum() => VectorMath.VectorSum(longArr);
+
+    [Benchmark]
+    public double DoubleSum() => doubleArr.Sum();
+
+    [Benchmark]
+    public double DoubleVectorSum() => VectorMath.VectorSum(doubleArr);
+
+    [Benchmark]
+    public float FloatSum() => floatArr.Sum();
+
+    [Benchmark]
+    public float FloatVectorSum() => VectorMath.VectorSum(floatArr);
+}
--- SimdTest.ConsoleApp/Program.cs
+++ SimdTest.ConsoleApp/Program.cs
@@ -1,33 +1,10 @@
-﻿using System.Diagnostics;
-using SimdTest.Lib;
+﻿using BenchmarkDotNet.Running;
+using SimdTest.ConsoleApp;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
-        var random = new Random();
-
-        var arr = new int[random.Next(0, 1000000)];
-
-        for (int i = 0; i < arr.Length; i++)
-        {
-            arr[i] = random.Next(1, 10);
-        }
-
-        var stopwatch = new Stopwatch();
-
-        var previousElapsed = 0L;
-
-        stopwatch.Start();
-
-        var sum1 = arr.Sum();
-        Console.WriteLine(previousElapsed = stopwatch.ElapsedTicks - previousElapsed);
-        Console.WriteLine(sum1);
-
-        var sum2 = VectorMath.VectorSum(arr);
-        Console.WriteLine(previousElapsed = stopwatch.ElapsedTicks - previousElapsed);
-        Console.WriteLine(sum2);
-
-        stopwatch.Stop();
+        var summary = BenchmarkRunner.Run<BenchmarkSumVsVectorSum>();
     }
 }
