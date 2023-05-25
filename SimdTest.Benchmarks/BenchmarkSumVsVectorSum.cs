a624635909bedca81da56a2fae8ccd3c8d98d593
Dmitry Zhbankov <dmitry.zhbankov@gmail.com>
2023-05-25 02:15:28 +0200

    Add ForEachSum benchmarks

--- SimdTest.Benchmarks/BenchmarkSumVsVectorSum.cs
+++ SimdTest.Benchmarks/BenchmarkSumVsVectorSum.cs
@@ -43,10 +43,16 @@ public class BenchmarkSumVsVectorSum
     public int IntVectorSum() => VectorMath.VectorSum(intArr);
 
     [Benchmark]
+    public int IntForEachSum() => ForEachSum(intArr);
+
+    [Benchmark]
     public long LongSum() => longArr.Sum();
 
     [Benchmark]
     public long LongVectorSum() => VectorMath.VectorSum(longArr);
+
+    [Benchmark]
+    public long LongForEachSum() => ForEachSum(longArr);
 
     [Benchmark]
     public double DoubleSum() => doubleArr.Sum();
@@ -55,8 +61,62 @@ public class BenchmarkSumVsVectorSum
     public double DoubleVectorSum() => VectorMath.VectorSum(doubleArr);
 
     [Benchmark]
+    public double DoubleForEachSum() => ForEachSum(doubleArr);
+
+    [Benchmark]
     public float FloatSum() => floatArr.Sum();
 
     [Benchmark]
     public float FloatVectorSum() => VectorMath.VectorSum(floatArr);
+
+    [Benchmark]
+    public float FloatForEachSum() => ForEachSum(floatArr);
+
+    private int ForEachSum(int[] arr)
+    {
+        var sum = default(int);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+
+    private long ForEachSum(long[] arr)
+    {
+        var sum = default(long);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+
+    private double ForEachSum(double[] arr)
+    {
+        var sum = default(double);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+
+    private float ForEachSum(float[] arr)
+    {
+        var sum = default(float);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        return sum;
+    }
 }
